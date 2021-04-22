7a902453d8bc576bf159b363b7e7b7f06ca8e645
Angel Mgl <angelemegeele@gmail.com>
2021-04-22 18:00:45 -0400

    About section created

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
-const { src, dest, series, watch } = require('gulp');
+const { src, dest, series, watch } = require("gulp");
+const imagemin = require("gulp-imagemin");
+const notify = require("gulp-notify");
+const webp = require("gulp-webp");
 /* const sass = require('gulp-sass'); */
+
+const paths = {
+    img: "src/img/**/*"
+}
 
 //compile sass
 /* function css() {
@@ -22,6 +29,25 @@ const { src, dest, series, watch } = require('gulp');
     watch("src/scss/app.scss", css);
 } */
 
+// this function compress image
+function images() {
+    return src(paths.img)
+        .pipe(imagemin())
+        .pipe(dest("./build/img"))
+        .pipe(notify({ message: "Image minified" }));
+}
+
+// this function convert images to webp
+function towebp() {
+    return src(paths.img)
+        .pipe(webp())
+        .pipe(dest("./build/img"))
+        .pipe(notify({ message: "Converted to webp" }));
+}
+
 /* exports.css = css;
 exports.minCss = minCss;
 exports.watchFiles = watchFiles; */
+exports.images = images;
+
+exports.default = series( images, towebp );
