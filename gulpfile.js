c89d52ee748f8c1e31c40973934a3ce66fcf0962
Angel Mgl <angelemegeele@gmail.com>
2021-04-22 13:00:28 -0400

    Sass settings setup

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const { src, dest, series, watch } = require('gulp');
+/* const sass = require('gulp-sass'); */
+
+//compile sass
+/* function css() {
+    return src("./src/scss/app.scss")
+        .pipe(sass())
+        .dest("./build/css")
+} */
+
+//minify css code
+/* function minCss() {
+    return src("./src/scss/app.scss")
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .dest("./build/css")
+} */
+
+//listening changes
+/* function watchFiles() {
+    watch("src/scss/app.scss", css);
+} */
+
+/* exports.css = css;
+exports.minCss = minCss;
+exports.watchFiles = watchFiles; */
