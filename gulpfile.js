4909e4991782d9058a1d9619160ccc66a45b9c7d
Angel Mgl <angelemegeele@gmail.com>
2021-04-23 01:03:27 -0400

    Gulp pluggins added and files fixed

--- gulpfile.js
+++ gulpfile.js
@@ -3,34 +3,43 @@ const imagemin = require("gulp-imagemin");
 const notify = require("gulp-notify");
 const webp = require("gulp-webp");
 const concat = require("gulp-concat");
-/* const sass = require('gulp-sass'); */
+const rename = require("gulp-rename");
+//const sass = require("gulp-sass");
+
+// CSS utilities
+//doesn't work without gulp-sass
+//const autoprefixer = require("autoprefixer");
+//const postcss = require("gulp-postcss");
+//const cssnano = require("cssnano");
+const sourcemaps = require("gulp-sourcemaps");
+
+//sourcemaps guarda la referencia original de los archivos minificados
+//para poder leer la consola incluso con el código minificado
+
+// JS utilities
+const terser = require("gulp-terser-js");
 
 const paths = {
     img: "src/img/**/*",
-    js: "src/js/**/*.js"
-}
+    scss: "./src/scss/app.scss",
+    js: "src/js/**/*.js",
+};
 
-//compile sass
-/* function css() {
-    return src("./src/scss/app.scss")
+//compile and minify sass
+/*function css() {
+    return src(paths.scss)
+        .pipe(sourcemaps.init()) 
         .pipe(sass())
-        .dest("./build/css")
-} */
-
-//minify css code
-/* function minCss() {
-    return src("./src/scss/app.scss")
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }))
-        .dest("./build/css")
-} */
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write())
+        .dest("./build/css");
+}*/
 
 //listening changes
 function watchFiles() {
     //watch("src/scss/app.scss", css);
     watch(paths.js, javascript);
-} 
+}
 
 // this function compress image
 function images() {
@@ -51,15 +60,18 @@ function towebp() {
 //minify javascript code
 function javascript() {
     return src(paths.js)
-        .pipe(concat('bundle.js'))
+        .pipe(sourcemaps.init())
+        .pipe(concat("bundle.js"))
+        .pipe(terser())
+        .pipe(sourcemaps.write('.'))
+        .pipe(rename({suffix: ".min"}))
         .pipe(dest("./build/js"));
 }
 
-/* exports.css = css;
-exports.minCss = minCss;*/
-exports.watchFiles = watchFiles; 
+//exports.css = css;
+exports.watchFiles = watchFiles;
 exports.images = images;
 exports.towebp = towebp;
 exports.javascript = javascript;
 
-exports.default = series( javascript, images, towebp, watchFiles );
+exports.default = series(javascript, images, towebp, watchFiles);
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,35 @@
-console.log('ready');
+document.addEventListener('DOMContentLoaded', () => {
+    scrollNav();
+
+    navFix();
+});
+
+function scrollNav(e) {
+    const links = document.querySelectorAll("#nav > a");
+    
+    links.forEach(link => {
+        link.addEventListener("click", e => {
+            e.preventDefault();
+            const section = document.querySelector(e.target.attributes.href.value);
+
+            section.scrollIntoView({
+                behavior: "smooth"
+            });
+        });
+    });
+}
+
+function navFix() {
+    const $nav = document.querySelector('.header');
+
+    // create an observer with IntersectionObserver API
+    const observer = new IntersectionObserver((entries) => {
+        let isVisible = entries[0].isIntersecting;
+
+        if(isVisible) $nav.classList.remove("nav-fixed");
+        else if(!isVisible && window.scrollY > 400) $nav.classList.add("nav-fixed");
+    });
+
+    // select element 
+    observer.observe(document.querySelector(".about"));
+}
